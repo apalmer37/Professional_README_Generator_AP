683ed7c5d0dc695be86c42568debd1e025504a5e
Alex Palmer <alexpalmer37@gmail.com>
2021-09-21 21:53:56 -0400

    more cleanup

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-// const fileGenerator = require('./util/fileGenerator');
 
 // create writeFile function using promises 
 const writeFileAsync = util.promisify(fs.writeFile);
